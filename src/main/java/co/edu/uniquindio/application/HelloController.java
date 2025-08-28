29f45f11038b26eb596d82d973d0ff06bde369a0
jecayonm8 <jecm0468@gmail.com>
2025-08-28 08:14:29 -0500

    chore: trigger reindex

--- src/main/java/co/edu/uniquindio/application/HelloController.java
+++ src/main/java/co/edu/uniquindio/application/HelloController.java
@@ -3,8 +3,18 @@ package co.edu.uniquindio.application;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.management.Notification;
+
 @RestController
 public class HelloController {
     @GetMapping("/hola")
-    public String hola() { return "¡Listo, Spring Boot + Gradle!"; }
+    public String hola() {
+        return "¡Listo, Spring Boot + Gradle!";
+    }
+
+//    @GetMapping("/mensaje")
+//    public String mensaje(){
+//        return "Hola mundo de proyecto springboot y gradle";
+//    }
+
 }
